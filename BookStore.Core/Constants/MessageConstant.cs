ff78f9dc3227884aa04922e67eb8d0a1a3cb0588
Blagovest Napoleonov <blgv@abv.bg>
2022-11-05 21:37:18 +0200

    Added Toatr

---
+++ BookStore.Core/Constants/MessageConstant.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Core.Constants
+{
+    public static class MessageConstant
+    {
+        public const string ErrorMessage = "ErrorMessage";
+        public const string WarningMessage = "WarningMessage";
+        public const string SuccessMessage = "SuccessMessage";
+    }
+}
